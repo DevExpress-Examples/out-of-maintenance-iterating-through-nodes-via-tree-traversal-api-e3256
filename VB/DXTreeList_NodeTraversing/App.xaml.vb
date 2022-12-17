1960166d00619df08fed7a7230fadc22b0b3c82a
DevExpressExampleBot <fakeEmail>
2022-12-17 15:07:24 +0400

    Source auto update [skip ci]

---
+++ VB/DXTreeList_NodeTraversing/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXTreeList_NodeTraversing
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/DXTreeList_NodeTraversing/MainWindow.xaml.vb
+++ VB/DXTreeList_NodeTraversing/MainWindow.xaml.vb
@@ -1,27 +1,28 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace DXTreeList_NodeTraversing
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			treeListControl1.ItemsSource = Stuff.GetStuff()
-		End Sub
 
-		Private Sub treeListControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			SmartExpandNodes(4)
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub SmartExpandNodes(ByVal minChildCount As Integer)
-			Dim nodeIterator As New TreeListNodeIterator(treeListView1.Nodes, True)
-			Do While nodeIterator.MoveNext()
-				nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount
-			Loop
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.treeListControl1.ItemsSource = GetStuff()
+        End Sub
+
+        Private Sub treeListControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            SmartExpandNodes(4)
+        End Sub
+
+        Private Sub SmartExpandNodes(ByVal minChildCount As Integer)
+            Dim nodeIterator As TreeListNodeIterator = New TreeListNodeIterator(Me.treeListView1.Nodes, True)
+            While nodeIterator.MoveNext()
+                nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount
+            End While
+        End Sub
+    End Class
 End Namespace
